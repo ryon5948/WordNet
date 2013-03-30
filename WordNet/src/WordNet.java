ee4238055b4d19ee51b82aa43f44e9edb02a725e
aryon <nueton5000@gmail.com>
2013-03-30 10:17:34 -0700

    Adding addRelations and parseNouns to WordNet constructor

--- WordNet/src/WordNet.java
+++ WordNet/src/WordNet.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,24 +12,34 @@ public class WordNet
 	// Class that contains the relationships
 	private class Synset 
 	{
-		List<Integer> relations;
-		String word;
+		private ArrayList<Integer> relations = new ArrayList<Integer>();
+		private String noun;
+		private String glossary;
 		
-		public Synset ()
+		public Synset (String noun, String glossary) 
 		{
-			this.relations = new ArrayList<Integer>();
-			word = null;
-		}
-		
-		public Synset (String word) 
-		{
-			this.word = word;
-			this.relations = new ArrayList<Integer>();
+			this.noun = noun;
+			this.glossary = glossary;
 		}
 		
 		public void addRelation(int a)
 		{
 			relations.add(a);
+		}
+		
+		public String getNoun()
+		{
+			return this.noun;
+		}
+		
+		public String getGlossary()
+		{
+			return this.glossary;
+		}
+		
+		public ArrayList<Integer> getRelations()
+		{
+			return this.relations;
 		}
 	}
 	
@@ -35,50 +47,100 @@ public class WordNet
 	private HashMap<Integer,Synset> wordNet = new HashMap<Integer,Synset>();
 	
 	// constructor takes the name of the two input files
-	public WordNet(String synsets, String hypernyms)
+	public WordNet(String synsets, String hypernyms) throws IOException
 	{
-		BufferedReader reader = new BufferedReader(new FileReader(synsets));
+		BufferedReader reader1 = new BufferedReader(new FileReader(synsets));
+		BufferedReader reader2 = new BufferedReader(new FileReader(hypernyms));
 		String line;
 		String[] wordSet = new String[3];
 		
 		// Creates a hashmap of synsets
-		while ((line = reader.readLine()) != null) {
-			  wordSet = this.parseLine(line);
-			  wordNet.put(Integer.getInteger(wordSet[0]),new Synset(wordSet[1]));
+		while ((line = reader1.readLine()) != null) {
+			  wordSet = this.parseNouns(line);
+			  wordNet.put(Integer.getInteger(wordSet[0]),new Synset(wordSet[1],wordSet[2]));
 			}
-		reader.close();
+		reader1.close();
+		
+		// Adds relationships to the hashmap
+		while ((line = reader2.readLine()) != null) {
+			  this.addRelations(wordNet, line);
+			}
+		reader1.close();
+		
 	}
 	
-	private static String[] parseLine(String line)
+	/**
+	 * Takes the line read from the file and parses it into the three different sections
+	 * used to create the WordNet.
+	 * @param line
+	 * @return
+	 */
+	private static String[] parseNouns(String line)
 	{
 			String[] pWords = new String[3];
 			int i = 0;
 			
-			while(line != null && !line.isEmpty())
+			while(line != null && !line.isEmpty() && i < 3)
 			{
 				int space = line.indexOf(' ');
 				String tLine = line.substring(0,space);
 				line = line.substring(space);
-				pWords[i] = tline;
+				pWords[i] = tLine;
 			}
 			
 			return pWords;
 	}
-
+	
+	private static void addRelations(HashMap<Integer, Synset> wn, String line)
+	{
+			ArrayList<Integer> relations = new ArrayList<Integer>();
+			boolean isFirst = true;
+			
+			while(line != null && !line.isEmpty())
+			{
+				Synset s = null;
+				int space = line.indexOf(' ');
+				String word = line.substring(0,space);
+				line = line.substring(space);
+				
+				// Word is the id
+				if(isFirst)
+				{
+					s = wn.get(word);
+				}
+				// Word is the id of a relation
+				else
+				{
+					s.addRelation(Integer.getInteger(word));
+				}
+			}
+	}
+/*
 	// returns all WordNet nouns
-	public Iterable<String> nouns()
+	public Iterable<String> nouns(){}
 
 	// is the word a WordNet noun?
-	public boolean isNoun(String word)
+	public boolean isNoun(String word){}
 
 	// distance between nounA and nounB (defined below)
-	public int distance(String nounA, String nounB)
+	public int distance(String nounA, String nounB){}
 
 	// a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
 	// in a shortest ancestral path (defined below)
-	public String sap(String nounA, String nounB)
+	public String sap(String nounA, String nounB){}
 
-	// for unit testing of this class
+	// for unit testing of this class*/
 	public static void main(String[] args)
+	{
+		WordNet wn = null;
+		try {
+			wn = new WordNet("nouns.txt","synset.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(wn);
+	}
 	
 }
